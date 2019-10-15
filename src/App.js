67595915f3c8a94f63f0149a7518d60db642cf78
Kamenev Maxim <megaroks@gmail.com>
2019-10-15 21:57:35 +0600

    added alert

--- src/App.js
+++ src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home.page';
 import { About } from './pages/About.page';
-import { Navbar } from './components/Navbar.conponent';
+import { Navbar } from './components/Navbar.component';
+import { Alert } from './components/Alert.component';
+import { AlertState } from './context/alert/AlertState';
 
 function App() {
     return (
-        <BrowserRouter>
-		<Navbar />
-            <div className="container pt-4">
-                <Switch>
-                    <Route path="/" exact component={Home}></Route>
-                    <Route path="/about" component={About}></Route>
-                </Switch>
-            </div>
-        </BrowserRouter>
+        <AlertState>
+            <BrowserRouter>
+                <Navbar />
+                <div className="container pt-4">
+                    <Alert />
+                    <Switch>
+                        <Route path="/" exact component={Home}></Route>
+                        <Route path="/about" component={About}></Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </AlertState>
     );
 }
 
